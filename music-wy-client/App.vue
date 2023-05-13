2534f2290f14f0a41acb601dbe8fc5c66bc57cc3
Aaron <Aaron@gmail.com>
2023-05-13 17:17:49 +0800

    init

---
+++ music-wy-client/App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch--Lauch生命周期')
+		},
+		onShow: function() {
+			console.log('App Show--show生命周期')
+		},
+		onHide: function() {
+			console.log('App Hide--show生命周期')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ music-wy-client/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="content">
+		<h1>网易云音乐</h1>
+		<span class="iconfont icon-shouye"></span>
+	</view>
+</template>
+
+<script>
+import '@/common/css/iconfont.css' 
+
+</script>
+
+<style>
+	
+</style>
